abe73dc52d2195b7f6945ea6c0ec2a37ab861f8d
rednotsus <Jiayangc1@gmail.com>
2024-12-04 18:37:22 -0600

    asd

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", async function () {
   let response = await fetch(
     `https://card-api.ch3n.cc/card/info/balance?number=${localStorage.getItem(
       "number"
-    )}}`
+    )}`
   );
   let data = await response.json();
   document.getElementById("money").innerText = data.balance;
